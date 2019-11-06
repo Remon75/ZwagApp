c117d0a8bff9566a1448b70b2c7e405dcfa6609b
Remon <remon.ghaly75@gmail.com>
2019-11-06 23:01:24 +0200

    initial commit

---
+++ ZwagApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ZwagApp.API.Models;
+
+namespace ZwagApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ ZwagApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZwagApp.API.Models
+{
+    public class Value
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
